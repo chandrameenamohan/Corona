dae904c2aaf607fce6f0dcb48a31e5ca2de3d3be
chandra mohan <chandrameenamohan@gmail.com>
2020-03-20 17:01:09 +0530

    Initial templete of code for rest-service

---
+++ src/main/java/org/embryyo/corona/service/CoronaService.java
@@ -0,0 +1,11 @@
+package org.embryyo.corona.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoronaService {
+    public static void main(String[] args) {
+        SpringApplication.run(CoronaService.class,args);
+    }
+}
